0ca99279ff3e933e788fc7d043a03666e25a5118
Wiglol <Wiggo.santti@gmail.com>
2024-11-19 09:46:34 +0200

    Done

---
+++ Upgifter/U.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = "25"; 
+
+            int number = int.Parse(numberAsString); 
+
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
