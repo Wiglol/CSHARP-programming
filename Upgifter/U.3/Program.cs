2c84b361dd316a65706254619ab0566f3ce26da4
Wiglol <Wiggo.santti@gmail.com>
2024-11-19 10:02:39 +0200

    New Upgift

---
+++ Upgifter/U.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: "); 
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + "!"); 
+
+
+        }
+    }
+}
