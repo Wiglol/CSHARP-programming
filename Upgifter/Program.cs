17cdd30d2a396b9122286af3ec024c0faed7cb34
Wiglol <Wiggo.santti@gmail.com>
2024-11-19 09:25:40 +0200

    Add files via upload

---
+++ Upgifter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Upgifter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String name = "Wiglo"; 
+
+            Console.WriteLine(name);
+
+
+
+            int age = 16;
+
+            int num = 21; 
+
+            Console.WriteLine(age);
+
+            Console.WriteLine(num);
+
+            var length = 190; 
+
+            Console.WriteLine(length);
+
+            Console.ReadKey(); 
+
+
+
+        }
+    }
+}
