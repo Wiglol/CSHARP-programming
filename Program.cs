a7b99370a0abcaaee18d03e0688dab25790ee252
Wiglol <Wiggo.santti@gmail.com>
2024-11-20 13:00:31 +0200

    Super done U.4

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          
+            Console.Write("How old are you human: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You can vote");
+            }
+            else
+            {
+                Console.WriteLine("You can't vote ");
+            }
+
+            Console.ReadLine();
+            
+            
+            
+           
+
+
+
+
+        }
+    }
+}
