c8f4f3e56b5ca0197f06aa6b72485cccd1311510
Wiglol <Wiggo.santti@gmail.com>
2024-11-20 13:27:29 +0200

    Done

---
+++ Upgifter/U.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many times do you want to loop: ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Sorry, Please enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine("Im better");
+                }
+
+                Console.WriteLine("loop finnished");
+            }
+
+
+
+
+        }
+    }
+}
