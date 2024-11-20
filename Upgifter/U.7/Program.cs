a72b4a7ae2400e30b1d30470f5b76c1f5220a1d4
Wiglol <Wiggo.santti@gmail.com>
2024-11-20 13:46:45 +0200

    U.7 too hard )=

---
+++ Upgifter/U.7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._7_True
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //defines counter variable
+            int Counter = 1;
+            while (Counter <= 5)
+            {//While Loop
+                Console.WriteLine("Counter: " + Counter);
+                Counter++;
+
+              
+            }
+            Console.WriteLine("The loop has finished!");
+        }
+    }
+}
