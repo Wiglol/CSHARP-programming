055d41ac4940d403f65c8daf9dc6473e20511de3
Wiglol <Wiggo.santti@gmail.com>
2024-11-25 13:36:06 +0200

    U.12 added

---
+++ Upgifter/U.12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a password: ");
+            string password = Console.ReadLine();
+            Console.Write("Please enter your passord again: ");
+            string password2 = Console.ReadLine();
+            
+            if
+              (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(password2))
+                Console.Write("please enter a password");
+            else if (password.Length < 6 || password2.Length < 6)
+                Console.Write("Password too short");
+            else if (password == password2)
+                Console.Write("password match");
+            else Console.Write("Passwords do not match"); 
+
+
+           
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
