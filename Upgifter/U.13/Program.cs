4f80c47832037cdc397d3860bed7c15238c0650b
Wiglol <Wiggo.santti@gmail.com>
2024-11-26 10:45:54 +0200

    U.13 added

---
+++ Upgifter/U.13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+                Console.WriteLine("Your number is odd");
+            Console.Write(number);
+
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
