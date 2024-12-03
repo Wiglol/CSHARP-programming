0fec9568fb36c462646ca00c92e97f9993b9abdd
Wiglol <Wiggo.santti@gmail.com>
2024-12-03 13:34:17 +0200

    U.21

---
+++ Upgifter/U.21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age");
+            String AgeText = Console.ReadLine();
+            int age  = Convert.ToInt32(AgeText);
+
+            
+            
+
+            
+
+            if (age >= 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+            else if (age >= 30)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("Over 18");
+            }
+            else if (age <= 17)
+            {
+                Console.WriteLine("Your are under 18"); 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Upgifter/U.21/READMEEEE.txt
@@ -0,0 +1,8 @@
+Namnet hade någo fel tror jag 
+
+this wasen't there  else if (age <= 17)
+            {
+                Console.WriteLine("Your are under 18"); 
+            }
+
+and other stuff but now good (= 
