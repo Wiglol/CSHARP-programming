745225eb33928825f2796ed235dbddca6abfa01d
Wiglol <Wiggo.santti@gmail.com>
2024-12-03 12:53:23 +0200

    U.20

---
+++ Upgifter/U.20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._20
+{
+    public class Circle
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Circle myCircle = new Circle(1.5);
+
+            
+            double area = myCircle.CalculateArea();
+
+          
+            Console.WriteLine($"Area of the circle is: {area}");
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+}
