34e0c35daae340947f1d9e7bb22528008037ef59
Wiglol <Wiggo.santti@gmail.com>
2024-12-03 12:30:40 +0200

    U.19

---
+++ Upgifter/U.19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] SumOfNumbers = { 0, 1, 2, 3, 4, 5 };
+            int sum = 0;
+            for (int i = 0; i < SumOfNumbers.Length; i++)
+            {
+                sum += SumOfNumbers[i];
+            }
+
+
+            if (sum >= -1) Console.WriteLine($"The sum is: {sum}");
+            else Console.WriteLine("Cannot add up an empty array");
+
+            int length = SumOfNumbers.Length;
+
+         
+            Console.WriteLine("The length of the array is: " + length);
+            Console.ReadLine();
+
+
+
+
+
+            
+
+
+        }
+    }
+}
---
+++ Upgifter/u.18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the width of your triangle: "); 
+            string widthText = Console.ReadLine();  
+            int width = Convert.ToInt32(widthText);
+            Console.Write("Please enter the height of your triangle: "); 
+            string heightText = Console.ReadLine();
+            int height = Convert.ToInt32(heightText);
+
+            Double area = (height * width) / 2.0;
+
+            Console.WriteLine($"The area of the triangle is: {area}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
