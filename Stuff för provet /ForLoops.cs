505050cead8f29c15e5ca593facfa1d21434cf8b
Wiglol <Wiggo.santti@gmail.com>
2024-12-16 17:42:04 +0200

    stuff

---
+++ Stuff för provet /ForLoops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter how many loops you want: ");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Ez 5 from provet");
+            }
+
+        }
+    }
+}
---
+++ Stuff för provet /IfElse+Switches.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learinng_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //U.4 
+
+
+            //Console.Write("Please enter your age: "); 
+
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("You can vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are tooooo young");
+            //}
+
+
+            // U.5
+
+            //Console.Write("Please enter what grade you got");
+            //int grade = Convert.ToInt32(Console.ReadLine());
+
+            //switch (grade)
+            //{
+            //    case 5:
+            //        Console.WriteLine("Excellent");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Good"); 
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Satisfactory"); 
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Ok"); 
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("Needs Improvement"); 
+            //        break;
+            //    case 0:
+            //        Console.WriteLine("Fail"); 
+            //        break;
+            //    default:
+            //        Console.WriteLine("Pick a number between 0-5");
+            //        break;
+
+
+
+
+            //}
+
+            
+
+           
+
+        }
+    }
+}
