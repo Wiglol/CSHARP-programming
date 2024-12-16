b4b3ed247a4ed384bf5fa5a420f363e7e8dff733
Wiglol <Wiggo.santti@gmail.com>
2024-12-16 18:41:44 +0200

    h

---
+++ Stuff för provet /Arrays combined with ForLoops.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somethinh_ome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] movies = new string[5];
+
+            Console.Write("Type in your five best movies from 0-4 0 is best: ");
+
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                movies[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine( "\nHere they are "); 
+
+            Array.Sort( movies );
+
+            for ( int i = 0; i < movies.Length; i++ )
+            {
+                Console.WriteLine( movies[i] );
+            }
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stuff för provet /The perfect thing about Lists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somethinh_ome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>();
+
+            shoppingList.Add("Dreams");
+            shoppingList.Add("Myname");
+            shoppingList.Add("Rainbows");
+            shoppingList.Add("Pony"); 
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
+            shoppingList.Remove("Dreams");
+            shoppingList.RemoveAt(2);
+
+            Console.WriteLine("---------");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(shoppingList[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
