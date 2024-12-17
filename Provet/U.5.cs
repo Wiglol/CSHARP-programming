9756d4e18548f925b059aa5d3cf6407695c610ff
Wiglol <Wiggo.santti@gmail.com>
2024-12-17 13:26:10 +0200

    5

---
+++ Provet/U.5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_U._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word and it's description: ");
+            Dictionary<string, string> Words = new Dictionary<string, string>
+            {
+              
+            }; 
+
+            Console.ReadLine();
+        }
+    }
+}
