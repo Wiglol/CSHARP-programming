9f4ce853a7f46f4444ae55e13181e6a56f15e03f
Wiglol <Wiggo.santti@gmail.com>
2024-12-18 09:40:32 +0200

    j

---
+++ Provet/U.2.2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provet_U._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            // Makes it so the console dosen't crash
+            if (double.TryParse(userInput, out double number)) 
+            {
+                if (number > 0)
+                {
+                    Console.WriteLine("The number is positive.");
+                }
+                else if (number == 0)
+                {
+                    Console.WriteLine("It's zero.");
+                }
+                else
+                {
+                    Console.WriteLine("The number is negative.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Can't do that! Please enter a valid number.");
+            }
+
+            Console.ReadLine(); // Keeps the console open and clean
+        }
+    }
+    
+}
---
+++ Provet/U.3.3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prov_U._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    // Makes an Array
+            string[] Names = { "Alice", "John", "Mia" };
+
+            Array.Sort(Names);
+            // For loop 
+            for (int i = 0; i < Names.Length; i++)
+            { // makes it say goodbye before the names
+                Console.WriteLine("Hello " + Names[i]);
+            }
+            // Array of numbers
+            int[] myNumbers = { 12, 45, 78, 3, 99, 21, 8 };
+            Array.Sort(myNumbers); // sorts the numbers in ascending oreder 
+            int numToRemove = 99;// removes the number 99 
+            int numIndex = Array.IndexOf(myNumbers, numToRemove);
+            myNumbers = myNumbers.Where((val, idx) => idx != numIndex).ToArray();
+            foreach (int i in myNumbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
