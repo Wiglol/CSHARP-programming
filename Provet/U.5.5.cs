9de6d92dcfba22f612982e4c1851254f5b9c88cd
Wiglol <Wiggo.santti@gmail.com>
2024-12-18 11:32:29 +0200

    h

--- Provet/U.5.5.cs
+++ Provet/U.5.5.cs
@@ -1,59 +1,74 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-   using System;
-            using System.Collections.Generic;
-using System.Security;
-
-public class Program
+namespace U._17_true
 {
-    
-    public static void Main()
+    internal class Program
     {
-        // Create a dictionary where the key is a string and the value is an integer
-        Dictionary<string, string> Items = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {   // The dictionay stores items 
+            Dictionary<string, string> Items = new Dictionary<string, string>();
+            bool running = true;
+            // A while loop
+            while (running)
+            {
+                Console.WriteLine("1. Add Subject");
+                Console.WriteLine("2. Remove Subject");
+                Console.WriteLine("3. Show all");
+                Console.WriteLine("4. Exit");
 
-        // Adding elements
-        for (int i = 0; i < 1; i++)
-        {
-            Console.Write("Please enter a name of a item: ");
-            string name = Console.ReadLine();  // User input for name
+                string input = Console.ReadLine();  
+                // A switch case that let's you choose options 1-4
+                switch (input)
+                {
+                    case "1": // Adds item and description to the dictionary
+                        Console.Write("Enter the name of a new item: ");
+                        string ItemName = Console.ReadLine();
+                        Console.Write("Please enter the description of that item: ");
+                        string Description = Console.ReadLine();
 
-            Console.Write("Please enter a descrition for that item: ");
-            string Description = (Console.ReadLine());  // User input for Description
+                        Items.Add(ItemName, Description); 
+                        Console.WriteLine("Succesfully added to dictionary");
 
-            // Add to dictionary
-            Items.Add(name, Description);
+ 
+                        break;
+
+                    case "2": // Let's you remove a item from the dictionary 
+                        Console.Write("Please enter the name of the item you wish to remove: ");
+                       
+                        Console.WriteLine(Items.Remove(Console.ReadLine()) ? "Item removed." : "Item not found.");
+                       
+
+
+
+                        break;
+                    case "3": // Shows what items are in the dictionary
+                        Console.WriteLine("Here are the Item and it's description"); 
+                        foreach (var subject in Items)
+                        {
+                            Console.WriteLine($"Items: {subject.Key}, Description: {subject.Value}");
+                        }
+
+                        break;
+                    case "4": // Shows you Exit 
+                        Console.WriteLine("Exit");   
+                        running = false; 
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                       
+                }
+            }
+            Console.ReadLine();
         }
-
-        if (Items.ContainsKey(Console.ReadLine()))
-        {
-            Console.WriteLine("This item already exists. Do you wan to overwrite it? (yes/no)"); 
-        }
-        Console.Write("What item do you wish to remove? :  ");
-        string ItemToRemove = Console.ReadLine();
-        if (Items.ContainsKey(ItemToRemove)) // This can remove items
-        {
-            Items.Remove(ItemToRemove);
-            Console.WriteLine("The item has been removed");
-        }
-        else
-        {
-            Console.WriteLine("Item not found"); 
-        }
-        Console.WriteLine("\nHere are Your items:"); //Shows your items in the end 
-        foreach (var My in Items)
-        {
-            Console.WriteLine(My.Key + ": " + My.Value);
-        }
-        Console.WriteLine();
-        Console.WriteLine("To exit press anything twice"); // tells you to exit 
-
-
-        Console.ReadLine();
-
-        
-        
     }
 }
-    
+
+
+
+   
